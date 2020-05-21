55c3c04db4b6cf3864ee1f36eb48653fdedb0c62
JoaoCalem <45393138+JoaoCalem@users.noreply.github.com>
2020-05-21 17:14:08 +0100

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,9 +1,5 @@
 import tkinter as tk
 
-def entry_click(name):
-    print(name)
-    #ent[name].delete(0, tk.END)
-
 window = tk.Tk()
 window.title = ("Encounter Builder")
 
@@ -13,16 +9,33 @@ ent = {}
 fr_PC = tk.Frame(window)
 
 lbl_PC = tk.Label(fr_PC, text="Player Stats")
+class Ent:
+    def __init__(self, name):
+        self.name = name
+        self.Default = True
+        ent[name] = tk.Entry(fr_PC,
+            width=5,
+            fg="#bdbdbd",
+            disabledforeground = "black",
+            disabledbackground = "white") 
+        ent[name].insert(0, "1")
+        ent[name].bind("<Button-1>", self.click)
+        ent[name].bind("<Return>", self.enter)
+        ent[name].grid(row=1, column=1)
 
-name = "PC_Level"
-ent[name] = tk.Entry(fr_PC, width=5, fg="#636363")
-ent[name].insert(0, "1")
-ent[name].bind("<Button-1>", lambda f=name: entry_click(f))
-lbl_PC_Level = tk.Label(fr_PC, text=("Level"))
+    def click(self,event):
+        if self.Default == True or ent[self.name]["state"] == "disabled":
+            ent[self.name]["state"] = "normal"
+            ent[self.name].delete(0, tk.END)
+            ent[self.name]["fg"] = "black"
+        self.Default = False
+    def enter(self,event):
+        ent[self.name]["state"] = "disabled"
 
+PC_Level = Ent("PC_Level")
 lbl_PC.grid(row=0, column=0, columnspan=2)
+lbl_PC_Level = tk.Label(fr_PC, text=("Level"))
 lbl_PC_Level.grid(row=1, column=0)
-ent[name].grid(row=1, column=1)
 
 #NPC
 fr_NPC = tk.Frame(window)
