2f2dac0b423706e9d9adcb84f100f3f8e575ba7c
JoaoCalem <45393138+JoaoCalem@users.noreply.github.com>
2020-05-21 19:23:12 +0100

    PC Stats 1.0 complete

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,49 +1,64 @@
 import tkinter as tk
 
+class Ent:
+    def __init__(self, row, name, default, frame):
+        self.name = name
+        self.value = default
+        self.state = "Default"
+        self.lbl = tk.Label(frame, text=(self.name), width=7, anchor="e")
+        self.ent = tk.Entry(frame,
+            width=2,
+            fg="#bdbdbd",
+            justify="right",
+            disabledforeground = "black",
+            disabledbackground = "white") 
+        self.ent.insert(0, self.value)
+        self.ent.bind("<Button-1>", self.click)
+        self.ent.bind("<Return>", self.enter)
+        self.lbl.grid(row=row, column=0, stick="e", pady=4)
+        self.ent.grid(row=row, column=1, pady=4, padx=6)
+
+    def click(self,event):
+        if self.state == "Default" or self.ent["state"] == "disabled":
+            self.ent["state"] = "normal"
+            self.ent.delete(0, tk.END)
+            self.ent["fg"] = "black"
+        self.state = "Custom"
+    def enter(self,event):
+        self.state = self.ent.get()
+        self.ent["state"] = "disabled"
+
 window = tk.Tk()
 window.title = ("Encounter Builder")
+window.minsize(200,100)
+window.columnconfigure(0, weight=1)
+window.columnconfigure(1, weight=1)
+window.rowconfigure(0, weight=1)
 
 ent = {}
 
 #PC
 fr_PC = tk.Frame(window)
+fr_PC_Stats = tk.Frame(fr_PC, relief="groove", borderwidth=1)
 
-lbl_PC = tk.Label(fr_PC, text="Player Stats")
-class Ent:
-    def __init__(self, name):
-        self.name = name
-        self.Default = True
-        ent[name] = tk.Entry(fr_PC,
-            width=5,
-            fg="#bdbdbd",
-            disabledforeground = "black",
-            disabledbackground = "white") 
-        ent[name].insert(0, "1")
-        ent[name].bind("<Button-1>", self.click)
-        ent[name].bind("<Return>", self.enter)
-        ent[name].grid(row=1, column=1)
+PC_Number = Ent(1,"Number", 4, fr_PC_Stats)
+PC_Level = Ent(2,"Level", 1, fr_PC_Stats)
+PC_Damage = Ent(3,"Damage", 7, fr_PC_Stats)
+PC_Hp = Ent(4,"HP", 8, fr_PC_Stats)
+PC_To_Hit = Ent(5,"To Hit", 5, fr_PC_Stats)
 
-    def click(self,event):
-        if self.Default == True or ent[self.name]["state"] == "disabled":
-            ent[self.name]["state"] = "normal"
-            ent[self.name].delete(0, tk.END)
-            ent[self.name]["fg"] = "black"
-        self.Default = False
-    def enter(self,event):
-        ent[self.name]["state"] = "disabled"
-
-PC_Level = Ent("PC_Level")
-lbl_PC.grid(row=0, column=0, columnspan=2)
-lbl_PC_Level = tk.Label(fr_PC, text=("Level"))
-lbl_PC_Level.grid(row=1, column=0)
+lbl_PC = tk.Label(fr_PC, text="PC STATS", font=("Segoe", 11))
+lbl_PC.grid(row=0, column=0, sticky="n")
+fr_PC_Stats.grid(row=2, column=0, sticky="n", pady=5)
 
 #NPC
 fr_NPC = tk.Frame(window)
-lbl_NPC = tk.Label(fr_NPC, text="Enemy Stats")
+lbl_NPC = tk.Label(fr_NPC, text="Enemy Stats", height=2, anchor="n")
 lbl_NPC.pack()
 
 #Layout
-fr_PC.grid(row=0, column=0)
-fr_NPC.grid(row=0, column=1)
+pad_Main = 10
+fr_PC.grid(row=0, column=0, sticky="e", padx=pad_Main, pady=pad_Main)
+fr_NPC.grid(row=0, column=1, sticky="w", padx=pad_Main, pady=pad_Main)
 
 window.mainloop()
