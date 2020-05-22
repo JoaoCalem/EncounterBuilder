6c7fb57d652a1edd5c2600c87b36ab30ea85c62a
JoaoCalem <45393138+JoaoCalem@users.noreply.github.com>
2020-05-22 04:18:44 +0100

    Monster Stats

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,11 +1,12 @@
 import tkinter as tk
 
 class Ent:
-    def __init__(self, row, name, default, frame):
+    def __init__(self, row, name, default, frame, n):
         self.name = name
         self.value = default
         self.state = "Default"
-        self.lbl = tk.Label(frame, text=(self.name), width=7, anchor="e")
+        if not self.name == "":
+            self.lbl = tk.Label(frame, text=(self.name), width=7, anchor="e")
         self.ent = tk.Entry(frame,
             width=2,
             fg="#bdbdbd",
@@ -15,8 +16,11 @@ class Ent:
         self.ent.insert(0, self.value)
         self.ent.bind("<Button-1>", self.click)
         self.ent.bind("<Return>", self.enter)
-        self.lbl.grid(row=row, column=0, stick="e", pady=4)
-        self.ent.grid(row=row, column=1, pady=4, padx=6)
+        if not self.name == "":
+            self.lbl.grid(row=row, column=0, stick="e", pady=4)
+            self.ent.grid(row=row, column=1, pady=4, padx=6)
+        else:
+            self.ent.grid(row=0, column=row, pady=4, padx=20)
 
     def click(self,event):
         if self.state == "Default" or self.ent["state"] == "disabled":
@@ -25,40 +29,56 @@ class Ent:
             self.ent["fg"] = "black"
         self.state = "Custom"
     def enter(self,event):
-        self.state = self.ent.get()
+        self.value = self.ent.get()
         self.ent["state"] = "disabled"
+
+
+class Monster:
+    def __init__(self,n):
+        self.fr = tk.Frame(fr_NPC_Stats)
+        self.Number = Ent(0,"", 1, self.fr, n)
+        self.Damage = Ent(1,"", 10, self.fr, n)
+        self.HP = Ent(2,"", 20, self.fr, n)
+        self.To_Hit = Ent(3,"", 3, self.fr, n)
+        self.AC = Ent(4,"", 12, self.fr, n)
+        self.fr.grid(row=n,column=0)
 
 window = tk.Tk()
 window.title = ("Encounter Builder")
 window.minsize(200,100)
-window.columnconfigure(0, weight=1)
-window.columnconfigure(1, weight=1)
-window.rowconfigure(0, weight=1)
 
 ent = {}
 
 #PC
-fr_PC = tk.Frame(window)
-fr_PC_Stats = tk.Frame(fr_PC, relief="groove", borderwidth=1)
+fr_PC_Stats = tk.Frame(window, relief="groove", borderwidth=1)
 
-PC_Number = Ent(1,"Number", 4, fr_PC_Stats)
-PC_Level = Ent(2,"Level", 1, fr_PC_Stats)
-PC_Damage = Ent(3,"Damage", 7, fr_PC_Stats)
-PC_Hp = Ent(4,"HP", 8, fr_PC_Stats)
-PC_To_Hit = Ent(5,"To Hit", 5, fr_PC_Stats)
-
-lbl_PC = tk.Label(fr_PC, text="PC STATS", font=("Segoe", 11))
-lbl_PC.grid(row=0, column=0, sticky="n")
-fr_PC_Stats.grid(row=2, column=0, sticky="n", pady=5)
+PC_Number = Ent(1,"Number", 4, fr_PC_Stats, "")
+PC_Level = Ent(2,"Level", 1, fr_PC_Stats, "")
+PC_Damage = Ent(3,"Damage", 7, fr_PC_Stats, "")
+PC_Hp = Ent(4,"HP", 8, fr_PC_Stats, "")
+PC_To_Hit = Ent(5,"To Hit", 5, fr_PC_Stats, "")
+PC_AC = Ent(6,"AC", 13, fr_PC_Stats, "")
+lbl_PC = tk.Label(window, text="PC STATS", font=("Segoe", 11))
+lbl_PC.grid(row=0, column=0)
+fr_PC_Stats.grid(row=1, column=0, padx=5, pady=5, stick="nsew")
 
 #NPC
-fr_NPC = tk.Frame(window)
-lbl_NPC = tk.Label(fr_NPC, text="Enemy Stats", height=2, anchor="n")
-lbl_NPC.pack()
+fr_NPC_Stats = tk.Frame(window, relief="groove", borderwidth=1)
+
+fr_labels = tk.Frame(fr_NPC_Stats)
+labels = {0:"Number",1:"Damage",2:"HP",3:"To Hit",4:"AC"}
+lbl = {}
+for i in labels:
+    lbl[i] = tk.Label(fr_labels, text=labels[i], width=7)
+    lbl[i].pack(side=tk.LEFT)
+fr_labels.grid(row=0, column=0, padx=10, pady=5)
+
+M = {}
+M[1] = Monster(1)
+
+lbl_NPC = tk.Label(window, text="ENEMY STATS", font=("Segoe", 11))
+lbl_NPC.grid(row=0, column=1)
+fr_NPC_Stats.grid(row=1, column=1, padx=5, pady=5, stick="nsew")
 
 #Layout
-pad_Main = 10
-fr_PC.grid(row=0, column=0, sticky="e", padx=pad_Main, pady=pad_Main)
-fr_NPC.grid(row=0, column=1, sticky="w", padx=pad_Main, pady=pad_Main)
-
 window.mainloop()
